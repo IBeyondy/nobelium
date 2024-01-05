fbc7109ced97d76373148bccabdab61592a3bb00
IBeyondy <ibeyondy_xxd@163.com>
2024-01-05 23:30:13 +0800

    minor change

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'IBeyondy',
   author: 'IBeyondy',
   email: 'ibeyondy_xxd@163.com',
-  description: '',
+  description: '石遇记',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
